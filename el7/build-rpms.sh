3e2d4c1e7c70d97d4d38363fdbf10d40192f9f70
Irving Leonard <irving.leonard@modmed.com>
2020-12-11 21:01:11 -0500

    fix(build): better bash script

--- el7/build-rpms.sh
+++ el7/build-rpms.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-yum -y install rpmdevtools yum-utils && \
-rpmdev-setuptree && \
-mv *.spec rpmbuild/SPECS/ && \
-yum-builddep -y rpmbuild/SPECS/python38-altinstall-duoauthproxy.spec && \
-spectool -g -R rpmbuild/SPECS/python38-altinstall-duoauthproxy.spec &&\
-QA_RPATHS=$[ 0x0002|0x0010 ] rpmbuild -ba rpmbuild/SPECS/python38-altinstall-duoauthproxy.spec && \
-yum -y install rpmbuild/RPMS/x86_64/python38-altinstall-duoauthproxy-3.8.4-3.el7.x86_64.rpm && \
+yum -y install rpmdevtools yum-utils
+
+rpmdev-setuptree
+
+for spec_files in *.spec; do
+	
+	if [ -e "$spec_files" ]; then
+		mv *.spec rpmbuild/SPECS/
+	else
+		echo "No new specs files were found"
+	fi
+    
+    break
+done
+
+yum-builddep -y rpmbuild/SPECS/python38-altinstall-duoauthproxy.spec
+spectool -g -R rpmbuild/SPECS/python38-altinstall-duoauthproxy.spec
+QA_RPATHS=$[ 0x0002|0x0010 ] rpmbuild -ba rpmbuild/SPECS/python38-altinstall-duoauthproxy.spec
+
+yum -y install rpmbuild/RPMS/x86_64/python38-altinstall-duoauthproxy-3.8.4-5.el7.x86_64.rpm
 /usr/local/bin/python3 build-rpms.py --openssl-dist /usr/local/openssl/ --show-output 1.el7
